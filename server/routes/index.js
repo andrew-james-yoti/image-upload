7751c1d43f615d1d3af31f213ba911fec4509a5c
andrew-james-yoti <andrew.james@yoti.com>
2020-01-07 17:14:14 +0000

    first commit

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+const upload = require('../services/upload');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/image-upload', upload.single('image'), (req, res) => {
+    res.status(200).json({ 'success': true });
+});
+
+module.exports = router;
